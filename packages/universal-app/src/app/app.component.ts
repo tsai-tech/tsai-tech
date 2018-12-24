d9b2d6542db8c57d4045a5c20a7c8caf86ef92a4
Mikhail Tsai <mikhail.tsai@gmail.com>
2018-12-24 10:11:19 +0300

    Initial commit.

---
+++ packages/universal-app/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+
+const STATE_KEY_APP = makeStateKey('app');
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  private DEFAULT_TITLE = 'Default title';
+  title: string;
+
+  constructor(
+    private http: HttpClient,
+    private state: TransferState
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.title = this.state.get(STATE_KEY_APP, this.DEFAULT_TITLE);
+    console.log('after 1-th get:', this.state.get(STATE_KEY_APP, '----'));
+
+    if (this.title === this.DEFAULT_TITLE) {
+      this.http.get('https://jsonplaceholder.typicode.com/todos/1')
+        .subscribe((result: any) => {
+          this.title = result.title;
+          this.state.set(STATE_KEY_APP, result.title);
+          console.log('after set:', this.state.get(STATE_KEY_APP, '----'));
+        });
+    }
+  }
+}
